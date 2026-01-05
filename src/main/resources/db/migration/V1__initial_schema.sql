0acc3181e4d2d5063f0aed0620ce597c71ac6675
Fabrizio Scandola <scandola.fabrizio@gmail.com>
2026-01-05 17:52:15 +0100

    Group deleted V6 and V7 migrations in initial migration

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,62 +1,117 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+CREATE TABLE users
+(
+    id            UUID PRIMARY KEY,
+    email         TEXT      NOT NULL UNIQUE,
+    username      TEXT      NOT NULL UNIQUE,
+    password_hash TEXT      NOT NULL,
+    created_at    TIMESTAMP NOT NULL,
+    updated_at    TIMESTAMP NOT NULL
 );
 
-CREATE TABLE projects (
-    id UUID PRIMARY KEY,
-    owner_id UUID NOT NULL,
-    name TEXT NOT NULL UNIQUE,
+CREATE TABLE projects
+(
+    id         UUID PRIMARY KEY,
+    owner_id   UUID      NOT NULL,
+    name       TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
+
+    CONSTRAINT uq_projects_name
+        UNIQUE (name),
+
     CONSTRAINT fk_projects_owner
-        FOREIGN KEY (owner_id) REFERENCES users(id)
+        FOREIGN KEY (owner_id)
+            REFERENCES users (id)
+            ON DELETE RESTRICT -- force explicit deletion
 );
 
-CREATE TABLE environments (
-    id UUID PRIMARY KEY,
-    project_id UUID NOT NULL,
-    name TEXT NOT NULL,
+CREATE TABLE environments
+(
+    id         UUID PRIMARY KEY,
+    project_id UUID      NOT NULL,
+    name       TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
 
     CONSTRAINT fk_env_project
-        FOREIGN KEY (project_id) REFERENCES projects(id),
+        FOREIGN KEY (project_id)
+            REFERENCES projects (id)
+            ON DELETE CASCADE,
 
     CONSTRAINT uq_env_project_name
         UNIQUE (project_id, name)
 );
 
-CREATE TABLE api_keys (
-    id UUID PRIMARY KEY,
-    environment_id UUID NOT NULL,
-    api_key TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+CREATE TABLE api_keys
+(
+    id             UUID PRIMARY KEY,
+    environment_id UUID      NOT NULL,
+    api_key        TEXT      NOT NULL UNIQUE,
+    created_at     TIMESTAMP NOT NULL,
+    updated_at     TIMESTAMP NOT NULL,
 
     CONSTRAINT fk_api_env
-        FOREIGN KEY (environment_id) REFERENCES environments(id)
+        FOREIGN KEY (environment_id) REFERENCES environments (id)
 );
+
+-- ==== CONFIG ENTRIES ====
+CREATE TYPE config_type_enum AS ENUM ('BOOLEAN', 'STRING', 'NUMBER', 'JSON');
 
 CREATE TABLE config_entries
 (
-    id UUID PRIMARY KEY,
-    environment_id UUID NOT NULL,
-    config_key TEXT NOT NULL,
-    config_value TEXT NOT NULL,
-    config_type TEXT NOT NULL CHECK (config_type IN ('int', 'float', 'bool', 'string')),
-    active_from TIMESTAMP,
-    active_until TIMESTAMP,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    id             UUID PRIMARY KEY,
+    environment_id UUID             NOT NULL,
+    config_key     TEXT             NOT NULL,
+    config_value   TEXT             NOT NULL,
+    config_type    config_type_enum NOT NULL,
+    active_from    TIMESTAMP,
+    active_until   TIMESTAMP,
+    created_by     UUID,
+    created_at     TIMESTAMP        NOT NULL,
+    updated_at     TIMESTAMP        NOT NULL,
 
     CONSTRAINT fk_config_env
-        FOREIGN KEY (environment_id) REFERENCES environments(id),
+        FOREIGN KEY (environment_id)
+            REFERENCES environments (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_config_created_by
+        FOREIGN KEY (created_by)
+            REFERENCES users (id),
 
     CONSTRAINT uq_config_env_key
-            UNIQUE (environment_id, config_key)
+        UNIQUE (environment_id, config_key),
+
+    CONSTRAINT chk_config_entry_valid_window
+        CHECK (
+            active_from IS NULL
+                OR active_until IS NULL
+                OR active_from <= active_until
+            )
+);
+
+CREATE INDEX idx_config_entry_key
+    ON config_entries (config_key);
+
+
+CREATE TABLE config_entry_history
+(
+    id                 UUID PRIMARY KEY,
+    config_entry_id    UUID      NOT NULL,
+    environment_id     UUID      NOT NULL,
+    changed_by         UUID      NOT NULL,
+    changed_at         TIMESTAMP NOT NULL,
+    change_description TEXT,
+    config_snapshot    TEXT     NOT NULL,  -- this should become JSONB at some point. Made it TEXT to simplify testing with H2
+
+    CONSTRAINT fk_history_entry
+        FOREIGN KEY (config_entry_id) REFERENCES config_entries (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_history_env
+        FOREIGN KEY (environment_id) REFERENCES environments (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_history_user
+        FOREIGN KEY (changed_by) REFERENCES users (id)
 );
